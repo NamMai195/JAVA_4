ef50e6c364f8047885633df769095dec5fcdadc5
nammai <nammai@pop-os>
2024-10-26 15:16:37 +0700

    Lab 1.1

--- Lab1/src/main/java/com/lab1/test/UserTest.java
+++ Lab1/src/main/java/com/lab1/test/UserTest.java
@@ -10,26 +10,30 @@ public class UserTest {
     public static void main(String[] args) {
         UserManager userManager = new UserManager();
 
-        // Thêm một số người dùng mới
-        User user1 = new User();
-        user1.setFullname("Nguyen Van D");
-        user1.setEmail("nguyenvand@fpt.edu.vn");
-        user1.setPassword("password101");
-        user1.setAdmin(false);
-        userManager.addUser(user1);
+        try {
+            // Thêm một số người dùng mới
+            User user1 = new User();
+            user1.setFullname("Nguyen Van D");
+            user1.setEmail("nguyenvand@fpt.edu.vn");
+            user1.setPassword("password101");
+            user1.setAdmin(false);
+            userManager.addUser(user1);
 
-        // Lấy tất cả người dùng
-        List<User> allUsers = userManager.findAll();
-        allUsers.forEach(user -> System.out.println(user.getFullname()));
+            // Lấy tất cả người dùng
+            List<User> allUsers = userManager.findAll();
+            allUsers.forEach(user -> System.out.println(user.getFullname()));
 
-        // Tìm kiếm người dùng không phải admin với email @fpt.edu.vn
-        List<User> fptUsers = userManager.findNonAdminFptUsers();
-        fptUsers.forEach(user -> System.out.println(user.getFullname() + " - " + user.getEmail()));
+            // Tìm kiếm người dùng không phải admin với email @fpt.edu.vn
+            List<User> fptUsers = userManager.findNonAdminFptUsers();
+            fptUsers.forEach(user -> System.out.println(user.getFullname() + " - " + user.getEmail()));
 
-        // Lấy người dùng theo trang
-        List<User> pageUsers = userManager.getUsersByPage(2, 5); // Trang 3
-        pageUsers.forEach(user -> System.out.println(user.getFullname()));
-
-        userManager.close();
+            // Lấy người dùng theo trang
+            List<User> pageUsers = userManager.getUsersByPage(2, 5); // Trang 3
+            pageUsers.forEach(user -> System.out.println(user.getFullname()));
+        } catch (Exception e) {
+            e.printStackTrace(); // In ra thông tin lỗi
+        } finally {
+            userManager.close(); // Đảm bảo rằng tài nguyên được đóng lại
+        }
     }
 }
